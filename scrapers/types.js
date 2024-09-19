fc3669d5d2863f57921b620abd2153aeb1e33ca7
katherineoung <katherine.oung@gmail.com>
2024-09-19 13:39:43 -0500

    gitignore testing

--- scrapers/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * You should declare the common types for your scraper in this document.
- * Make sure to import your types in the main scraper document.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
